ef09ff3c5667f37b56dac43c15c94676ce7c6530
Ali Alrahahleh <aalraha@caq-vcat-2-r1.walmart.com>
2015-06-18 10:08:03 -0700

    init

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function which return encapsulated matrix
+## param variable of type matrix
+## return list
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(l) {
+    x <<- l
+    #reset the cache on change
+    inv <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  getInverce <- function () {
+    inv
+  }
+  setInverce <- function (i) {
+    inv <<- i
+  }
+  list(set = set, get = get, getInverce = getInverce, setInverce = setInverce)
 
 }
 
 
-## Write a short comment describing this function
+## Cache function use to compute the inverse of matrix 
+## return cached inverse of matrix if exist, or compute the inverse 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverce();
+  if(is.null(inv))
+  {
+    inv <- solve(x$get(), ...);
+  }
+  else
+  {
+    print("Getting cached data");
+  }
+  x$setInverce(inv);
+  inv
 }
